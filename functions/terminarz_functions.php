e27617989dc4d9241e8352d426d4281f1ac3a977
freken148 <pomanuk39@gmail.com>
2026-04-27 18:49:31 +0200

    plan for teminarz

---
+++ functions/terminarz_functions.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        Terminarz
+        - Add and remove events by teacher who have lessons with certain class in certain day (except absence, it's should be possible to add in any day)
+        - See events by months based on dates, not day of week like in real librus
+        - Teacher must have to choice a type of event or absence, and range of lessons that it take
+        - All types must be seen in lesson plan, except absence
+        - Types of events: 
+        sprawdzian, kartkówka, nieobecność, zastępstwo, informacja, inne, wywiadówka
+        - Supervisor of the class must have full access to change timetable events
+    */
+?>
